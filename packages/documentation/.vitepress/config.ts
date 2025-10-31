35c8a313b2530ffae23db1eb312c113fc3cc5949
d3xter666 <yavor.ivanov@sap.com>
2025-10-31 11:18:00 +0200

    refactor: Dynamic docs upload configuration

--- packages/documentation/.vitepress/config.ts
+++ packages/documentation/.vitepress/config.ts
@@ -7,7 +7,8 @@ import MarkdownItImplicitFigures from "markdown-it-implicit-figures";
 
 export default defineConfig({
 
-  base: "/cli/v5/", // GitHub Pages deployment base path
+  // Would be set in CI job via CLI arguments. For local development, it's just root.
+  base: "/",
   srcDir: "docs",
   outDir: "dist",
   lang: "en-US",
@@ -274,7 +275,7 @@ function guide() {
     },
     {
       text: "API Reference",
-      link: "https://ui5.github.io/cli/api/index.html",
+      link: "./api/index.html",
 
     },
 
